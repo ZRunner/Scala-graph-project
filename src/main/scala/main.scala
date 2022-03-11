3d23bb37ddab6b8fdc63a3715aa44c527a016e45
Arthur <blaisearth@cy-tech.fr>
2022-03-11 15:23:24 +0100

    1st test uwu

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,12 @@
+import undirected.{Edge, SimpleGraphDefaultImpl}
+
 object Main extends App {
     println("Hello, world")
+
+    var graph = new SimpleGraphDefaultImpl[Int](Set(1, 2, 3, 4), Set(Edge(1, 2), Edge(3, 2), Edge(1, 4)))
+
+    graph = graph + 5
+    graph = graph +| Edge(4, 5)
+
+    println(graph.neighborsOf(4))
 }
