f9fa13b760508439e16c17d1b412dc2f4727063e
ZRunner <z.runner.mc@gmail.com>
2022-03-20 18:14:55 +0100

    push frequencies tests

--- src/main/scala/applications/Frequencies.scala
+++ src/main/scala/applications/Frequencies.scala
@@ -1,5 +1,7 @@
 package applications
 
+import undirected.{Edge, SimpleGraphDefaultImpl}
+
 object Frequencies extends App {
     def time[R](block: => R): R = {
         val t0 = System.nanoTime().toDouble
@@ -11,5 +13,20 @@ object Frequencies extends App {
 
     println("hello world from frequencies")
 
-    println(Utils.readFile().head)
+    val antennes = Utils.readFile()
+    println(s"${antennes.size} antennes lues")
+
+    def get_edges(antennes: Set[Antenne]) : Set[Edge[Antenne]] = {
+        antennes.zipWithIndex.map{ case (a, i) => {
+            antennes.slice(i, antennes.size).filter(b => {
+                a != b && (a.coo.dg_lat - b.coo.dg_lat) < 2 && (a.coo.dg_long - b.coo.dg_long) < 2 && (a.coo - b.coo) < 2
+            }).map(b => Edge(a, b))
+        }}.flatten
+    }
+
+    // println(time { get_edges(antennes.slice(0, 10_000)).size } )
+
+    val g = SimpleGraphDefaultImpl(antennes.slice(0, 6_000), get_edges(antennes.slice(0, 6_000)))
+    println("g is created")
+    println(time {g.coloringDSATUR} )
 }
