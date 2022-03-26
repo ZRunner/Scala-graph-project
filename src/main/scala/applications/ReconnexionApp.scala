4311feec00a89d0123e638f94caff700d150bcc1
ZRunner <z.runner.mc@gmail.com>
2022-03-26 11:50:40 +0100

    better Reconnexion app

--- src/main/scala/applications/ReconnexionApp.scala
+++ src/main/scala/applications/ReconnexionApp.scala
@@ -12,23 +12,34 @@ object Reconnexion extends App {
         result
     }
 
+    /** Filtre une liste d'antennes pour supprimer celles ayant les mêmes coordonnées */
+    def filterUniqueCoo(list: Set[Antenne]) : Set[Antenne] = {
+        list.groupBy(_.coo).map(_._2.head).toSet
+    }
+
+    /** Récupère la distance entre deux antennes (valeur de l'arête) pour chaque couple d'antennes du graphe */
     def get_values(antennes: Set[Antenne]) : Map[Edge[Antenne], Double] = {
         antennes.zipWithIndex.map{ case (a, i) => {
             antennes.slice(i+1, antennes.size).map(b => (Edge(a, b) -> (a.coo - b.coo)))
         }}.flatten.toMap
     }
 
-    val antennes = Utils.getAntennes().filter(_.commune == "77075")
+    val antennes = filterUniqueCoo(Utils.getAntennes().filter(_.commune == "62570"))
     println(s"${antennes.size} antennes lues")
 
     val g = SimpleGraphDefaultImpl(antennes, Set[Edge[Antenne]]())
-    println("Graphe créé")
     val valuation = get_values(antennes)
+    println("Graphe créé")
 
     val tree = g.withAllEdges.minimumSpanningTree(valuation)
     println("Liste des antennes :")
     tree.vertices.map(x => println("\t"+x))
     println("Liaison des antennes :")
-    tree.edges.map(x => println("\tAntenne "+x._1.id+" et antenne "+x._2.id+" ("+(x._1.coo-x._2.coo)+"km de distance)"))
+    tree.edges.map(x => println("\tAntenne "+x._1.id+" et antenne "+x._2.id+" (%.3f km de distance)".format(x._1.coo-x._2.coo)))
     println("Distance totale : "+tree.value(valuation))
+
+
+    /* Affiche le nombre d'antennes ayant des coordonnés différentes pour chaque commune */
+    // println()
+    // Utils.getAntennes().groupBy(_.commune).map(g => (g._1 -> filterUniqueCoo(g._2).size)).filter(_._2 > 8).map(x => println("\t"+x))
 }
